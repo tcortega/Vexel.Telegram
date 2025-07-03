fc01652b817a9cbfcc5d98d4b200cafe804546f1
Tarcisio Ortega <github.squishier824@passmail.net>
2025-07-03 13:49:42 -0300

    chore: remove old committed bot token

--- samples/Vexel.Telegram.Sample/Program.cs
+++ samples/Vexel.Telegram.Sample/Program.cs
@@ -12,7 +12,7 @@ using Microsoft.Extensions.Logging;
 using Remora.Commands.Extensions;
 
 var host = Host.CreateDefaultBuilder(args)
-	.AddTelegramService(_ => "***REMOVED-BOT-TOKEN***")
+	.AddTelegramService(_ => "<BOT_TOKEN>")
 	.ConfigureServices((ctx, services) =>
 	{
 		_ = services.AddTelegramCommands();
